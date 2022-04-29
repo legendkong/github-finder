a13582404e690629b00b9bb817a274f171001d2c
Jonathan Kong <jonathankong96@hotmail.com>
2022-04-30 01:35:37 +0800

    Add User profile section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
 
 function Home() {
    return (
-      <div>
-         <h1 className="text-6xl">Welcome</h1>
-         {process.env.REACT_APP_GITHUB_URL}
-         {process.env.REACT_APP_GITHUB_TOKEN}
-      </div>
+      <>
+         <UserSearch />
+         <UserResults />
+      </>
    )
 }
 
